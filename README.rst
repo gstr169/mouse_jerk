6e9a4fd834ba871266305eaaa80175915b8f14a6
d.finko <d.finko@x5.ru>
2020-10-27 20:49:05 +0300

    added timeout arg

---
+++ README.rst
@@ -0,0 +1,7 @@
+Mouse Jerk
+-----------
+
+Installation:
+#############
+
+ python3 -m pip install mouse_jerk
--- mouse_jerk/clicker.py
+++ mouse_jerk/clicker.py
@@ -1,4 +1,5 @@
 from pynput import mouse
+import argparse
 
 
 def invisible_move(mouse_controller):
@@ -6,11 +7,21 @@ def invisible_move(mouse_controller):
     mouse_controller.move(-1, 0)
 
 
+def process_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-t', '--timeout', default=30, type=int,
+                        help='set timeout in seconds after last event')
+
+    args = parser.parse_args()
+    return args.timeout
+
+
 def listen_and_count():
+    timeout = process_args()
     mouse_controller = mouse.Controller()
     with mouse.Events() as events:
         while True:
-            event = events.get(30)
+            event = events.get(timeout)
             if event is None:
                 invisible_move(mouse_controller)
-                print('You did not interact with the mouse within one second')
+                print(f'You did not interact with the mouse within {timeout} seconds')
